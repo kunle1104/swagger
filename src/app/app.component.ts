1e1c98151ec30d8ff68fb59c80a906352d066203
Adekunle Ishola <kunle1104@gmail.com>
2019-03-10 18:19:51 +1100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,29 +23,21 @@ export class AppComponent implements OnInit, OnDestroy {
      this.carsSub = this.swaggerService.getCarsListener() // subscribe to listen to gotten cars
         .subscribe((c: CarShow[]) => {
           this.carShow = c;
-          for ( let i = 0; i < this.carShow.length; i++) { // sort all car arrays in carShow
+          for ( let i = 0; i < this.carShow.length; i++) { // sort arrays and group
             this.carShow[i].cars.sort(function(a, b) {
                 return (a.make < b.make) ? -1 : (a.make > b.make) ? 1 : 0;
             });
-            this.carGroups[i] =  _.groupBy(this.carShow[i].cars, 'make');
-            // console.log('Result :', this.carGroups[i]);
+            this.carGroups[i] =  _.groupBy(this.carShow[i].cars, 'make'); // groupby using _
             for (const x of Object.values(this.carGroups[i])) {
-              // console.log(x);
               this.carGroupArray[i] = x;
             }
-            this.groupKeys[i] = new Array();
+            this.groupKeys[i] = new Array();  // get {} keys to be able tooutput to HTML
             for (const x of Object.keys(this.carGroups[i])) {
-              // console.log(x);
               this.groupKeys[i].push(x);
             }
             console.log('groups :', i);
             console.log(this.groupKeys[i]);
           }
-             // console.log('Group 0: ', this.carGroups[0]);
-             // console.log('Group 1: ', this.carGroups[1]);
-
-            // console.log('grouped: ', this.carGroups);
-
      });
    }
    ngOnDestroy() {
